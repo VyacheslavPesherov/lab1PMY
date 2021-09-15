6c666d978f6ede7a7368258a8d4a15b45d7dc71e
VyacheslavPesherov <frobbyword@yandex.ru>
2021-09-15 13:09:16 +0700

    сим

--- app/src/test/java/com/example/lab1/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab1/ExampleUnitTest.java
@@ -14,8 +14,25 @@ public class ExampleUnitTest {
     public void addition_myMAX() {
         MainActivity m = new MainActivity();
         assertEquals(m.myMAX(4,5,9), 9);
+    }
+
+    @Test
+    public void addition_myMAX_ravno() {
+        MainActivity m = new MainActivity();
+
         assertEquals(m.myMAX(1,1,1), 1);
+    }
+
+    @Test
+    public void addition_myMAX_minus() {
+        MainActivity m = new MainActivity();
         assertEquals(m.myMAX(-3,-7,-2), -2);
+    }
+
+    @Test
+    public void addition_myMAX_vse() {
+        MainActivity m = new MainActivity();
+
         assertEquals(m.myMAX(4,-10,0), 4);
     }
 
@@ -23,8 +40,28 @@ public class ExampleUnitTest {
     public void addition_myMIN() {
         MainActivity m = new MainActivity();
         assertEquals(m.myMIN(4,5,9), 4);
+    }
+
+    @Test
+    public void addition_myMIN_ravno() {
+        MainActivity m = new MainActivity();
+
         assertEquals(m.myMIN(1,1,1), 1);
+
+    }
+
+    @Test
+    public void addition_myMIN_nol() {
+        MainActivity m = new MainActivity();
+
         assertEquals(m.myMIN(0,0,1), 0);
+    }
+
+    @Test
+    public void addition_myMIN_minus() {
+        MainActivity m = new MainActivity();
+
         assertEquals(m.myMIN(-10,-8,0), -10);
     }
+
 }
